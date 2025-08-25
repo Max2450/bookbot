20f377f8d7e7995bbfc5323179396c0602185647
Max2450 <mrg245000@gmail.com>
2025-08-25 15:11:46 -0700

    implemented sys for filepath handling

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#import sys to handle file paths using sys.argv
+import sys
+
 #import text analysis functions from stats.py
 from stats import get_word_count, get_char_count, sorted_char_count
 
@@ -9,18 +12,24 @@ def get_book_text(filepath):
 
 #main function to run the program. Prints report formatting. 
 def main():
+
+    if len(sys.argv) == 2:
+        book_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
     #header
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at /books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     #word count
     print("----------- Word Count ----------")
-    get_word_count(get_book_text("./books/frankenstein.txt"))
-    print(get_word_count(get_book_text("./books/frankenstein.txt")))
+    get_word_count(get_book_text(book_path))
+    print(get_word_count(get_book_text(book_path)))
     
     #character count
     print("--------- Character Count -------")
-    book_char_count = get_char_count(get_book_text("./books/frankenstein.txt"))
+    book_char_count = get_char_count(get_book_text(book_path))
 
     #sorted character count report
     alpha_char_count_report = sorted_char_count(book_char_count)
