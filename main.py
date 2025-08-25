72bccee6d7022fc350f3cf9e569901527984550e
Max2450 <mrg245000@gmail.com>
2025-08-25 11:31:27 -0700

    Added report formatting

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from stats import get_word_count, get_char_count
+#import text analysis functions from stats.py
+from stats import get_word_count, get_char_count, sorted_char_count
 
+#read in book text from file
 def get_book_text(filepath):
     with open(filepath) as f:
         book_text = f.read()
         return book_text
 
+#main function to run the program. Prints report formatting. 
 def main():
-    #print(get_book_text("./books/frankenstein.txt"))
+    
+    #header
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at /books/frankenstein.txt...")
+    #word count
+    print("----------- Word Count ----------")
     get_word_count(get_book_text("./books/frankenstein.txt"))
     print(get_word_count(get_book_text("./books/frankenstein.txt")))
+    
+    #character count
+    print("--------- Character Count -------")
     book_char_count = get_char_count(get_book_text("./books/frankenstein.txt"))
-    print(book_char_count)
+
+    #sorted character count report
+    alpha_char_count_report = sorted_char_count(book_char_count)
+
+    #footer  
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
+#takes in book text as a string and returns word count
 def get_word_count(book_text):
     words = book_text.split()
     word_count = len(words)
-    return f"{word_count} words found in the document"
+    return f"Found {word_count} total words"
 
+#takes in book text as a string and returns a dictionary with character counts
 def get_char_count(book_text):
     char_count = {}
     for char in book_text:
@@ -12,3 +14,24 @@ def get_char_count(book_text):
         else:
             char_count[lower_char] += 1
     return char_count
+
+#takes in character count dictionary and returns a sorted list of dictionaries with character and count
+def sorted_char_count(char_count):
+    sorted_report = []
+    
+    #create list of dictionaries from char_count dict
+    for char, num in char_count.items():
+        inner_dict = {}
+        inner_dict['char'] = char
+        inner_dict['num'] = num
+        sorted_report.append(inner_dict)
+
+    #sort list of dictionaries by 'num' key in descending order
+    sorted_report.sort(reverse=True, key=lambda d: d['num'])
+    
+    for dict in sorted_report:
+        if not dict['char'].isalpha():
+            continue
+        print(f"{dict['char']}: {dict['num']}")
+
+    return sorted_report
